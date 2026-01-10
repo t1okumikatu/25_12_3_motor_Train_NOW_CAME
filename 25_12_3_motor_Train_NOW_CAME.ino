5ffdeee84d896eb108833480d06c753eaaa9c653
tokumikatu <t1okumikatu@gmail.com>
2026-01-10 20:11:38 +0900

    カメラとcase文で前進停止

--- 25_12_3_motor_Train_NOW_CAME.ino
+++ 25_12_3_motor_Train_NOW_CAME.ino
@@ -1,7 +1,7 @@
 #include "esp_camera.h"
 #include <esp_now.h>
 #include <WiFi.h>
-#include "src/constant.h"
+#include "constant.h"
 
 // WARNING!!! PSRAM IC required for UXGA resolution and high JPEG quality
 //            Ensure ESP32 Wrover Module or other board with PSRAM is selected
